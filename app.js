43753ab070271f1b014275cf6316c64755412d28
SwaraGoyal <108713049+SwaraGoyal@users.noreply.github.com>
2023-12-04 11:18:55 +0530

    y

--- app.js
+++ app.js
@@ -1,64 +1,71 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+
+const app = express();
 
 // Make the MongoDB connection URL configurable
 const mongoDBUrl = process.env.MONGODB_URL || "mongodb://localhost:27017/studentregistration";
 mongoose.connect(mongoDBUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-var db = mongoose.connection;
+const db = mongoose.connection;
+
 db.on("error", console.error.bind(console, "connection error"));
 db.once("open", function () {
   console.log("Connected to MongoDB");
 });
 
-var app = express();
-
 app.use(bodyParser.json());
-app.use(express.static("public"));
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// CORS configuration
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.post("/sign_up", function (req, res) {
-  const full_name = req.body.full_name;
-  const roll_number = req.body.roll_number;
-  const email = req.body.email;
-  const birthdate = req.body.birthdate;
-  const address = req.body.address;
-  const skills = req.body.skills;
-  const interests = req.body.interests;
-  const family_income = req.body.family_income;
-  const course_choice = req.body.course_choice;
-  const post_college_plans = req.body.post_college_plans;
+  const {
+    full_name,
+    roll_number,
+    email,
+    birthdate,
+    address,
+    skills,
+    interests,
+    family_income,
+    course_choice,
+    post_college_plans,
+  } = req.body;
 
-  var data = {
-    full_name: full_name,
-    roll_number: roll_number,
-    email: email,
-    birthdate: birthdate,
-    address: address,
-    skills: skills,
-    interests: interests,
-    family_income: family_income,
-    course_choice: course_choice,
-    post_college_plans: post_college_plans,
+  const data = {
+    full_name,
+    roll_number,
+    email,
+    birthdate,
+    address,
+    skills,
+    interests,
+    family_income,
+    course_choice,
+    post_college_plans,
   };
 
   db.collection("students").insertOne(data, function (err, collection) {
-    if (err) throw err;
-    console.log("Record inserted Successfully");
-  });
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Internal Server Error");
+    }
 
-  return res.redirect("signup_success.html");
+    console.log("Record inserted Successfully");
+    return res.redirect('signup_success.html');
+  });
 });
 
 app.get("/", function (req, res) {
-  res.set({
-    "Access-control-Allow-Origin": "*",
-  });
-  return res.redirect("index.html");
+  return res.redirect('index.html');
 });
 
 // Use dynamic port or default to 3000
